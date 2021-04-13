3df2e52cbd9ed23b42dc421b7874a1c4001e0a6f
Yolshin <elshin195@gmail.com>
2021-04-13 20:24:43 +0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterpartyCreateComponent } from './counterparty-create/counterparty-create.component';
+import { NomenclatureCreateComponent } from './nomenclature-create/nomenclature-create.component';
+import { OrderCreateComponent } from './order-create/order-create.component';
+import { OrderLineComponent } from './order-line/order-line.component';
+import { OrderComponent } from './order/order.component';
+import { PriceCreateComponent } from './price-create/price-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create-order', component: OrderCreateComponent},
+  {path: 'create-nomenclature', component: NomenclatureCreateComponent},
+  {path: 'create-counterparty', component: CounterpartyCreateComponent},
+  {path: 'create-price', component: PriceCreateComponent},
+  {path: 'list-order', component: OrderLineComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
